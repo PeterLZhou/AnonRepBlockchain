215fc128bddacd89689fcd8cde3ec65969dc2eb6
dibyatanoy <dibyatanoy.bhattacharjee@yale.edu>
2017-11-22 01:42:00 -0500

    Created starter file for utility functions, util.py

---
+++ util.py
@@ -0,0 +1,15 @@
+import socket
+import threading
+from client import Client
+from server import Server
+import time
+import json
+import util
+
+def sendDict(dict, ip_addr, port):
+    print("Not implemented yet.")
+    return
+
+def readDict(timeout = 0.5):
+    print("Not implemented yet.")
+    # return dictionary_read, sender_ip, sender_port
