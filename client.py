cdfc78ff5c3527e63bdb448eb6c6460f0d8f79c6
PeterLZhou <peter.zhou@yale.edu>
2017-11-21 23:48:01 -0500

    Client creation starter code and some function skeletons

--- client.py
+++ client.py
@@ -1,10 +1,27 @@
 import socket
 import threading
+import uuid
 from random import *
 
 class Client():
-    def __init__(self):
-        # TODO: Add in crypto code for initializing clients
-        self.wallets = []
-        self.client_id = randint(1, 1000) 
-        print("Nothing really yet to do for client")
+    def __init__(self, clientid):
+        self.wallets = dict()
+        # Client ID is just the ID of the port it's on, since we only have 1 client/port
+        self.client_id = clientid
+        # Long standing private key
+        self.private_key = uuid.uuid4().hex
+
+    # Called by the server, returns a tuple of two new public keys for the wallet
+    def split(self, walletid):
+        oldwallet = None #TODO @Eugine - once you figure createwallet out find out how to get the wallet by reference
+        newwallet1 = self.createwallet()
+        newwallet2 = self.createwallet()
+        # TODO: Split currency among the new wallets
+        del wallets[oldwallet]
+        return (newwallet1, newwallet2)
+
+    # Adds the wallet to the dictionary. Returns the key for the wallet in the dict
+    def createwallet(self):
+        # TODO @Eugine: Add in crypto code for initializing wallets - the way I
+        # imagined it was a mapping of either publickey/privatekey/address to currency amt
+        print("Not implemented")
--- server.py
+++ server.py
@@ -11,11 +11,11 @@ class Server():
         self.sendsocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         #Used for the server to receive inbound messages
         self.receivesocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
+
+        ### SOCKET CREATION ###
+        # Creates a socket at localhost and next available 5000 client
         self.MY_IP = "0.0.0.0"
         self.MY_PORT = 5000
-        self.MY_MESSAGES = []
-        self.MY_CLIENTS = {}
-        offset = 0
         connected = False
         while not connected:
             try:
@@ -26,6 +26,18 @@ class Server():
                 print("Port {} is probably in use".format(self.MY_PORT))
                 self.MY_PORT += 1
 
+        ### CLIENT CREATION ###
+        # Everyone creates one client on startup
+        self.MY_CLIENTS = {}
+        new_client = Client(self.MY_PORT)
+        self.MY_CLIENTS[new_client.client_id] = new_client
+        print("Client {} added".format(new_client.client_id))
+        print("Client private key: ", new_client.private_key)
+        ### MESSAGE CREATION ###
+        # Everyone has their own log of messages, every time we get a message append to log
+        self.MY_MESSAGES = {}
+
+
     def listen(self):
         while True:
             data, addr = self.receivesocket.recvfrom(1024) # buffer size is 1024 bytes
@@ -35,17 +47,30 @@ class Server():
 
     # Send a message to every port which is not yourself
     def sendall(self):
+        client_id = "None"
         message = "TEST MESSAGE"
-        self.MY_MESSAGES.append(message)
+        self.MY_MESSAGES[client_id] = message
         for port in ALL_PORTS:
             if port != self.MY_PORT:
                 self.sendsocket.sendto(message.encode(), (self.MY_IP, port))
 
-    def dump(self):
-        for message in self.MY_MESSAGES:
-            print(message)
+    def postmessage(self, client_id, message):
+        self.MY_MESSAGES[client_id] = message
+        # TODO: Broadcast stuff
+
+    def broadcastmessages(self):
+        for client_id, message in self.MY_MESSAGES:
+            print("%s: %s", client_id, message)
 
     def newclient(self):
-        new_client = Client()
+        #TODO, decide on a naming scheme for client
+        new_client = Client(self.MY_PORT + str(rand(100)))
         self.MY_CLIENTS[new_client.client_id] = new_client
         print("Client {} added".format(new_client.client_id))
+
+    def upvote(self, client_id, message_id):
+        # TODO: Linkable ring signature + upvote
+        print("nope")
+    def downvote(self, client_id, message_id):
+        # TODO: Linkable ring signature + downvote
+        print("nope")
